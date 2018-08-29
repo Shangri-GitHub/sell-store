1b85fc0657fb61216b6ed267966cfe9dcee0e903
韩星 <467527309@qq.com>
2018-08-29 10:45:52 +0800

    搭建整个框架

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import navbar from '../components/nav-bar.vue'
+import discover from '../pages/discover/discover.vue'
+import cart from '../pages/cart/cart.vue'
+import me from '../pages/me/me.vue'
 import home from '../pages/home/home.vue'
+import relaxfood from '../pages/home/relax-food.vue'
+import clothes from '../pages/home/clothes.vue'
+
+
 
 Vue.use(Router)
 
@@ -15,19 +22,31 @@ export default new Router({
       path: '/navbar',
       component: navbar,
       children: [
-    //     {
-    //       path: 'relaxfood',
-    //       component: RelaxFood
-    //     },
+        {
+          path: 'relaxfood',
+          component: relaxfood
+        },
         {
           path: 'home',
           component: home
         },
-    //     {
-    //       path: 'clothes',
-    //       component: Clothes
-    //     },
+        {
+          path: 'clothes',
+          component: clothes
+        },
       ]
     },
+    {
+      path: '/discover',
+      component: discover
+    },
+    {
+      path: '/cart',
+      component: cart
+    },
+    {
+      path: '/me',
+      component: me
+    }
   ]
 })
