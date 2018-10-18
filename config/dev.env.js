55233f8e90be5449f831199d64d711afa16b6be8
韩星 <467527309@qq.com>
2018-10-18 21:07:12 +0800

    修改为自己的域名

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_ROOT: '"//127.0.0.1:8080/sell"'
+  API_ROOT: '"http://47.106.236.175:8080/sell"'
 })
